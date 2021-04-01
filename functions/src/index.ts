7e4dee7551fdfd4e4851f78b726ab9b515e832e1
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-01 14:53:44 +0700

    [Firebase] Fix CF conf & Firestore rules

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 // import { TBD } from '@phits-tech/common/dist/dao-firestore/schema'
 
-import functions from './_services/firebase-functions-initialized'
-import { getToken, getUrl } from './auth'
+// import functions from './_services/firebase-functions-initialized'
+// import { getToken, getUrl } from './auth'
 
 // HTTPS Callable
-export const authNUConnectURL = functions.https.onCall(getUrl)
-export const authNUConnectToken = functions.https.onCall(getToken)
+// export const authNUConnectURL = functions.https.onCall(getUrl)
+// export const authNUConnectToken = functions.https.onCall(getToken)
 
 // Firestore
 // export const tbd = functions.firestore.document(`${tbd}/tbd`).onCreate(tbd)
