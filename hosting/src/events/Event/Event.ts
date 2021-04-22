560b8c1e0779d7df682ff5282e8d080486135ebf
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-22 16:01:24 +0700

    [Events] Fix meta (title)

--- hosting/src/events/Event/Event.ts
+++ hosting/src/events/Event/Event.ts
@@ -2,8 +2,10 @@ import { computed } from '@vue/runtime-core'
 import { Vue } from 'vue-class-component'
 import { useMeta } from 'vue-meta'
 import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
 
 import { Route } from '~/router/route-decorator'
+import { storeKey } from '~/store'
 import { EventUi } from '../models'
 
 @Route({ name: 'Event', path: '/events/:slug', props: true })
@@ -11,9 +13,10 @@ export default class Event extends Vue.with(class {
   slug!: string
 }) {
   route = useRoute()
-  meta = useMeta(computed(() => ({ title: `Event ${this.route.params.slug as string}` })))
+  store = useStore(storeKey)
+  meta = useMeta(computed(() => ({ title: this.event ? this.event.name : 'Event not found' })))
 
   get event(): EventUi | undefined {
-    return this.$store.getters.eventBySlug(this.route.params.slug)
+    return this.store.getters.eventBySlug(this.slug)
   }
 }
