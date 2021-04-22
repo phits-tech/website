8e356d4f858e532eda3203fb1ef30cf071a738de
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-22 12:09:06 +0700

    [Event] Extend DAO model (to min sync effort)

--- hosting/src/events/models.ts
+++ hosting/src/events/models.ts
@@ -2,31 +2,20 @@ import dayjs, { Dayjs } from 'dayjs'
 
 import { Event } from '@phits-tech/common/dist/dao-firestore/model-types'
 
-export interface EventUi {
-  slug: string
-  name: string
-  description: string
+export type EventUi = Omit<Event, 'dateStart' | 'dateEnd'> & {
   dateStart: Dayjs
   dateEnd: Dayjs
   time: string
-  location: string
-  series?: string
-  seriesType?: string
-  badges?: string[]
-  hostId?: string
-  hostName?: string
-  registrationRequired?: boolean
-  registrationUrl?: string
 }
 
-export function eventToEventUi(event: Event): EventUi {
-  const dsDayjs = dayjs.unix(event.dateStart.seconds)
-  const deDayJs = dayjs.unix(event.dateEnd.seconds)
+export const eventToEventUi = (event: Event): EventUi => {
+  const dateStart = dayjs.unix(event.dateStart.seconds)
+  const dateEnd = dayjs.unix(event.dateEnd.seconds)
 
   return {
     ...event,
-    dateStart: dsDayjs,
-    dateEnd: deDayJs,
+    dateStart,
+    dateEnd,
     time: dsDayjs.format('ddd DD MMM, HH:mm') + '-' + deDayJs.format('HH:mm')
   }
 }
