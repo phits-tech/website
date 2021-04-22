cef0d9ae05adc3d50169821d60a2f0f2846f29d8
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-22 17:46:03 +0700

    [Meta] Add `og` tags (dormant)

--- hosting/src/events/Event/Event.ts
+++ hosting/src/events/Event/Event.ts
@@ -1,7 +1,6 @@
 import { computed } from '@vue/runtime-core'
 import { setup, Vue } from 'vue-class-component'
 import { useMeta } from 'vue-meta'
-import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 
 import { Route } from '~/router/route-decorator'
@@ -12,9 +11,15 @@ import { EventUi } from '../models'
 export default class Event extends Vue.with(class {
   slug!: string
 }) {
-  route = useRoute()
   store = useStore(storeKey)
-  meta = setup(() => useMeta(computed(() => ({ title: this.event?.name ?? 'Event not found' }))))
+
+  meta = setup(() => useMeta(computed(() => ({
+    title: this.event?.name ?? 'Event not found',
+    og: {
+      // TODO: This will only be relevant if we enable SSR
+      image: this.event?.bannerUrl ?? ''
+    }
+  }))))
 
   get event(): EventUi | undefined {
     return this.store.getters.eventBySlug(this.slug)
