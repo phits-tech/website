754f65462d733ea036ad732c3f5d341fb7c7035f
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-25 01:28:11 +0700

    [Routing] Only inject comp. to "SingleView"
    - Auto-naming for Component[] mode
    - DRY (merge flatmaps)
    - Readability (extract to named funcs)

--- hosting/src/router/route-decorator/route-builder.ts
+++ hosting/src/router/route-decorator/route-builder.ts
@@ -4,12 +4,19 @@ import { RouteRecordRaw } from 'vue-router'
 import { RouteBinding } from './route-binding'
 
 export const getRoutes = (components: { [name: string]: Component } | Component[]): RouteRecordRaw[] =>
+  asNamedEntries(components)
+    .flatMap(([name, component]) => getBindings(component).map(binding => createRoute(binding, name, component)))
+    .sort((r1, r2) => r2.priority - r1.priority)
+
+const asNamedEntries = (components: { [name: string]: Component } | Component[]): Array<[string, Component]> =>
   (Array.isArray(components))
-    ? components
-      .flatMap(component => (component.prototype.$routeBindings as RouteBinding[] ?? [])
-        .map(route => Object.assign({ component, priority: 0 }, route)))
-      .sort((r1, r2) => r2.priority - r1.priority)
+    ? Array.from(components.entries()).map(entry => [entry[0].toString(), entry[1]] as [string, Component])
     : Object.entries(components)
-      .flatMap(([name, component]) => (component.prototype.$routeBindings as RouteBinding[] ?? [])
-        .map(route => Object.assign({ name, component, priority: 0 }, route)))
-      .sort((r1, r2) => r2.priority - r1.priority)
+
+const getBindings = (component: Component): RouteBinding[] =>
+  component.prototype.$routeBindings ?? []
+
+const createRoute = (binding: RouteBinding, name: string, component: Component): RouteRecordRaw & { priority: number } =>
+  (binding.redirect !== undefined || binding.components !== undefined)
+    ? Object.assign({ name, priority: 0 }, binding) // Redirect or MultipleViews
+    : Object.assign({ name, priority: 0, component }, binding) // SingleView
