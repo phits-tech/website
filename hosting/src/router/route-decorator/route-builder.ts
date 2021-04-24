9315e91a8deb98720517497378a1b214401e2835
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-25 00:25:48 +0700

    [Routing] Clearer indentation & reject TODO

--- hosting/src/router/route-decorator/route-builder.ts
+++ hosting/src/router/route-decorator/route-builder.ts
@@ -3,12 +3,13 @@ import { RouteRecordRaw } from 'vue-router'
 
 import { RouteBinding } from './route-binding'
 
-// TODO: Use Component name as default if only 1 route
 export const getRoutes = (components: { [name: string]: Component } | Component[]): RouteRecordRaw[] =>
   (Array.isArray(components))
-    ? components.flatMap(component => (component.prototype.$routeBindings as RouteBinding[] ?? [])
-      .map(route => Object.assign({ component, priority: 0 }, route)))
+    ? components
+      .flatMap(component => (component.prototype.$routeBindings as RouteBinding[] ?? [])
+        .map(route => Object.assign({ component, priority: 0 }, route)))
       .sort((r1, r2) => r2.priority - r1.priority)
-    : Object.entries(components).flatMap(([name, component]) => (component.prototype.$routeBindings as RouteBinding[] ?? [])
-      .map(route => Object.assign({ name, component, priority: 0 }, route)))
+    : Object.entries(components)
+      .flatMap(([name, component]) => (component.prototype.$routeBindings as RouteBinding[] ?? [])
+        .map(route => Object.assign({ name, component, priority: 0 }, route)))
       .sort((r1, r2) => r2.priority - r1.priority)
