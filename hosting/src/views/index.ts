c82514a34604f1c303709a70cf336175ea5257a7
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-04-25 00:22:56 +0700

    [Components] Add comments

--- hosting/src/views/index.ts
+++ hosting/src/views/index.ts
@@ -1,4 +1,10 @@
+// Get all .vue files
 const components = require.context('@/', true, /\.vue$/i)
+
+// Export as { filename: component }
 export default Object.fromEntries(
-  components.keys().map(key => [key.match(/\/([^/]*)\./)?.[1] ?? '', components(key).default])
+  components.keys().map(key => [
+    key.match(/\/([^/]*)\./)?.[1] ?? '', // filename (without path & ext)
+    components(key).defaults // component (must be export default)
+  ])
 )
