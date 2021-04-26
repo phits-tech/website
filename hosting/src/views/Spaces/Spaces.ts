fc55225e0c344a7a595761da59789f85bf01a89d
Sopaphorn <82072741+Sopaphorn@users.noreply.github.com>
2021-04-26 17:42:26 +0700

    [Spaces]-add code for design the web page

--- hosting/src/views/Spaces/Spaces.ts
+++ hosting/src/views/Spaces/Spaces.ts
@@ -6,4 +6,15 @@ import { Route } from '@/router/route-decorator'
 @Route({ name: 'Spaces', path: '/spaces' })
 export default class Spaces extends Vue {
   meta = setup(() => useMeta({ title: 'Spaces' }))
+  spaces = [
+    {},
+    {},
+    {},
+    {
+      name: 'Maker Club, NU',
+      logo: '',
+      banner: 'https://images.unsplash.com/photo-1585980243496-fe29a36bd382',
+      spaceType: 'community'
+    }
+  ]
 }
