36af30d62932c0d71d98fefbd6970f4a4e3250d1
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-05-07 20:54:35 +0700

    [Deps] Alias @phits-tech/common

--- hosting/vue.config.js
+++ hosting/vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
+        "@phits-tech/common": path.resolve(__dirname, '../common/dist'),
         '@assets': path.resolve(__dirname, 'src/_assets'),
         '~': path.resolve(__dirname, 'src/_services')
       }
