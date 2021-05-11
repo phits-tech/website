1db6ac3099a2bcc2bfa6b93a0272ac85461de51c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-05-11 16:07:57 +0700

    [Lint] Move .prettierrc/.stylelintrc to root

---
+++ .stylelintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-property-sort-order-smacss'
+  ],
+  rules: {
+    // Override
+    'no-empty-source': undefined,
+    'order/properties-alphabetical-order': undefined,
+    'declaration-block-trailing-semicolon': undefined,
+
+    // Additional
+    'function-name-case': [
+      'lower',
+      { ignoreFunctions: [/^[a-z][\dA-Za-z]+$/] }
+    ],
+    'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind'] }],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['global', 'export'] }
+    ]
+  }
+}
