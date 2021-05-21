04c0bf9604c8f027050eb01b026ca0042cb551af
Antony Harfield <antonyharfield@gmail.com>
2021-05-21 10:44:50 +0700

    [Seeds] Remove tokens from seed urls

--- admin/src/test-data/data/spaces.ts
+++ admin/src/test-data/data/spaces.ts
@@ -8,8 +8,8 @@ export const spaces: Space[] = [
       en: 'A lab for working on hardware, IoT, Arduino and other engineering projects.',
       th: 'ห้องปฏิบัติการสำหรับทำงานเกี่ยวกับ hardware, IoT, Arduino และโครงการวิศวกรรมอื่น ๆ'
     },
-    banner: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fmaker-club-nu-banner.jpg?alt=media&token=9ccefce1-b0d2-4808-a4a3-109ed37a9194',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fmaker-club-nu-logo.jpg?alt=media&token=da3b874d-ba62-4bd9-b236-43cab188a51c',
+    banner: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/maker-club-nu-banner.jpg',
+    logo: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/maker-club-nu-logo.jpg',
     category: 'community',
     locationText: '4th floor SC2, Naresuan University',
     locationLatitude: 16.742_235_085_922_57,
@@ -22,8 +22,8 @@ export const spaces: Space[] = [
       en: 'A co-working space for individuals and groups, supported by Stock Exchange of Thailand. Also has a selection of boardgames!',
       th: 'พื้นที่ทำงานร่วมกันสำหรับบุคคลและกลุ่มที่ได้รับการสนับสนุนจากตลาดหลักทรัพย์แห่งประเทศไทย นอกจากนี้ยังมีเกมกระดานให้เลือกอีกด้วย'
     },
-    banner: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fsetic-nu-banner.jpg?alt=media&token=a5ce286e-37d1-4cbe-80ed-e14db07c4438',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fsetic-nu-logo.jpg?alt=media&token=66a5c132-087f-4ac4-a34d-5a40e68962d9',
+    banner: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/setic-nu-banner.jpg',
+    logo: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/setic-nu-logo.jpg',
     category: 'coworking',
     locationText: 'Near BEC, Naresuan University',
     locationLatitude: 16.748_156_316_592_908,
@@ -36,8 +36,8 @@ export const spaces: Space[] = [
       en: 'A coffee shop and small co-working space behind Engineering.',
       th: 'ร้านกาแฟและพื้นที่ทำงานร่วมกันขนาดเล็กด้านหลังวิศวกรรม'
     },
-    banner: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fusit-nu-banner.jpg?alt=media&token=4a5b2039-a513-48c3-9d26-719ddc8d97c2',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fusit-nu-logo.jpg?alt=media&token=c82966c5-28a3-4dd5-bdfe-5719257a7ddd',
+    banner: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/usit-nu-banner.jpg',
+    logo: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/usit-nu-logo.jpg',
     category: 'cafe',
     locationText: 'Near Engineering, Naresuan University',
     locationLatitude: 16.744_032_818_990_863,
@@ -50,8 +50,8 @@ export const spaces: Space[] = [
       en: 'A virtual meeting place to learn and socialize with the tech community in Phitsanulok.',
       th: 'สถานที่พบกันเสมือนจริงเพื่อเรียนรู้และสังสรรค์กับชุมชนเทคโนโลยีในพิษณุโลก'
     },
-    banner: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fmodern-technology.jpeg?alt=media&token=e440f586-b143-4ce0-9039-a1e03921e441',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fphits-tech-logo.png?alt=media&token=a3e436e1-f303-4083-b3d6-e324f4073e3c',
+    banner: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/discord-banner.png',
+    logo: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/phits-tech-logo.png',
     category: 'online',
     locationText: 'https://discord.gg/4CHb53F'
   },
@@ -62,8 +62,8 @@ export const spaces: Space[] = [
       en: 'A place to re-live streamed events run by Phits Tech.',
       th: 'สถานที่สำหรับถ่ายทอดสดกิจกรรมที่ดำเนินการโดย Phits Tech'
     },
-    banner: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Ftarget-marketing.jpeg?alt=media&token=aec127f6-e685-40de-a09c-416450b0dcf3',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/phits-tech-emu.appspot.com/o/spaces%2Fphits-tech-logo.png?alt=media&token=a3e436e1-f303-4083-b3d6-e324f4073e3c',
+    banner: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/youtube-banner.png',
+    logo: 'https://storage.googleapis.com/phits-tech-emu.appspot.com/spaces/phits-tech-logo.png',
     category: 'online',
     locationText: 'https://www.youtube.com/channel/UC83wlCFD2-FOYxSUzVG-oUg'
   }
