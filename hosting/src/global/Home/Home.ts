b21d25e6375a1a269443cb108d34f0c3aca3d24d
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-05-26 21:49:53 +0700

    [Locales] Home translations

--- hosting/src/global/Home/Home.ts
+++ hosting/src/global/Home/Home.ts
@@ -5,21 +5,28 @@ import { EventUi } from '@/events/models'
 import { Route } from '~/router/route-decorator'
 
 import HomeBanners from './HomeBanners.vue'
+import { messages } from './translations-home'
+
+import 'dayjs/locale/th'
 
 interface EventSummary { slug: string, name: string, dateStart: Dayjs, dateEnd: Dayjs, location: string }
 interface DayAndEvents { day: string, events: EventSummary[] }
 
 @Route({ path: '/' })
-@Options({ components: { HomeBanners } })
+@Options({
+  components: { HomeBanners },
+  i18n: { messages }
+})
 export default class Home extends Vue {
   get sevenDaysAhead(): DayAndEvents[] {
     const events: EventUi[] = this.$store.getters.events
-    const startOfDayLocal = dayjs().startOf('day')
+    console.log(this.$i18n.locale)
+    const startOfDayLocal = dayjs().locale(this.$i18n.locale).startOf('day')
 
     return Array.from({ length: 7 }, (_, i) => {
       const day = startOfDayLocal.add(i, 'day')
       return {
-        day: (i === 0) ? 'Today' : ((i === 1) ? 'Tomorrow' : day.format('ddd')),
+        day: (i === 0) ? this.$t('today') : ((i === 1) ? this.$t('tomorrow') : day.format('ddd')),
         events: events.filter(e => day.isBefore(e.dateStart) && day.add(1, 'day').isAfter(e.dateStart))
       }
     })
