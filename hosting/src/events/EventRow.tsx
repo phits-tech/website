18f52554f006f89295e11e37808cf389b5adae68
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-02 19:51:49 +0700

    [Deps] Bump

--- hosting/src/events/EventRow.tsx
+++ hosting/src/events/EventRow.tsx
@@ -1,9 +1,7 @@
 import { Vue } from 'vue-class-component'
-import { Prop } from 'vue-property-decorator'
 
 import { EventUi } from '../models'
 
-export default class EventRow extends Vue {
-  @Prop()
+export default class EventRow extends Vue.with(class {
   event!: EventUi
-}
+}) {}
