e08df362a5d2af6b581a648ec63b5dd6a180cc79
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-03 08:36:56 +0700

    [Heroes] Use i18n-t + interpolation
    - Merge translation pieces (now 1 translation with placeholders)
    - ...and yarn fixx

--- hosting/src/people/Heroes/translations-heroes.ts
+++ hosting/src/people/Heroes/translations-heroes.ts
@@ -1,43 +1,37 @@
+import { LocaleMessages } from '@intlify/core-base'
 import { mapValues } from 'lodash'
+import { VueMessageType } from 'vue-i18n'
 
 import { TranslatedString } from '@phits-tech/common/dao-firestore'
 
 const translations: { [key: string]: TranslatedString } = {
-  heroesBecomeTitle: {
+  becomeHeroTitle: {
     en: 'Become a Hero',
     th: 'การเป็นฮีโร่'
   },
-  heroesThe: {
-    en: 'The',
-    th: ''
+  becomeHeroText: {
+    en: 'The {linkCommunityHero} award is Phits.Tech\'s way to recognize and thank individuals for their contributions to our community. Every hour engaged in community events earns {boldCcus} (Community Contribution Units). The easiest way to become a Community Hero is to volunteer to run an event. Learn more about the {linkBenefits}, or get started now:',
+    th: 'รางวัล {linkCommunityHero} คือวิธีของ Phis.Tech ในการยกย่องและขอบคุณบุคคลต่างๆ ที่มีส่วนร่วมกับชุมชนของเรา ทุก ๆ ชั่วโมงที่มีส่วนร่วมในกิจกรรมชุมชนจะได้รับ {boldCcus} (Community Contribution Units) วิธีที่ง่ายที่สุดในการเป็นฮีโร่ชุมชนคือการอาสาจัดกิจกรรม เรียนรู้เพิ่มเติมเกี่ยวกับ {linkBenefits} หรือเริ่มต้นตอนนี้:'
   },
-  heroesCommunityHero: {
-    en: 'Community Hero',
-    th: 'รางวัล Community Hero'
-  },
-  heroesAward: {
-    en: "award is Phits.Tech's way to recognize and thank individuals for their contributions to our community. Every hour engaged in community events earns",
-    th: 'คือวิธีของ Phis.Tech ในการยกย่องและขอบคุณบุคคลต่างๆ ที่มีส่วนร่วมกับชุมชนของเรา ทุก ๆ ชั่วโมงที่มีส่วนร่วมในกิจกรรมชุมชนจะได้รับ'
-  },
-  heroesEasiest: {
-    en: 'The easiest way to become a Community Hero is to volunteer to run an event. Learn more about the',
-    th: 'วิธีที่ง่ายที่สุดในการเป็นฮีโร่ชุมชนคือการอาสาจัดกิจกรรม เรียนรู้เพิ่มเติมเกี่ยวกับ'
-  },
-  heroesBenefits: {
+  benefits: {
     en: 'benefits of becoming a hero',
     th: 'ประโยชน์ของการเป็นฮีโร่'
   },
-  heroesStarted: {
-    en: ', or get started now:',
-    th: ', หรือเริ่มต้นตอนนี้'
-  },
-  heroesYourContribution: {
+  yourContribution: {
     en: 'Your contribution',
     th: 'ผลงานของคุณ'
+  },
+  hallOfFame: {
+    en: 'Hall of Fame',
+    th: 'หอเกียรติยศ'
+  },
+  latestEvent: {
+    en: 'Last event',
+    th: 'เหตุการณ์ล่าสุด'
   }
 }
 
-export const messages = {
+export const messages: LocaleMessages<VueMessageType> = {
   en: mapValues(translations, vals => vals.en),
   th: mapValues(translations, vals => vals.th)
 }
