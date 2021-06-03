db9ccbe624eb5f9e507463b0a48fdb9839229cce
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-03 08:40:17 +0700

    [Heroes] Rename translations to group them

--- hosting/src/people/Heroes/translations-heroes.ts
+++ hosting/src/people/Heroes/translations-heroes.ts
@@ -13,7 +13,7 @@ const translations: { [key: string]: TranslatedString } = {
     en: 'The {linkCommunityHero} award is Phits.Tech\'s way to recognize and thank individuals for their contributions to our community. Every hour engaged in community events earns {boldCcus} (Community Contribution Units). The easiest way to become a Community Hero is to volunteer to run an event. Learn more about the {linkBenefits}, or get started now:',
     th: 'รางวัล {linkCommunityHero} คือวิธีของ Phis.Tech ในการยกย่องและขอบคุณบุคคลต่างๆ ที่มีส่วนร่วมกับชุมชนของเรา ทุก ๆ ชั่วโมงที่มีส่วนร่วมในกิจกรรมชุมชนจะได้รับ {boldCcus} (Community Contribution Units) วิธีที่ง่ายที่สุดในการเป็นฮีโร่ชุมชนคือการอาสาจัดกิจกรรม เรียนรู้เพิ่มเติมเกี่ยวกับ {linkBenefits} หรือเริ่มต้นตอนนี้:'
   },
-  benefits: {
+  becomeHeroTextBenefits: {
     en: 'benefits of becoming a hero',
     th: 'ประโยชน์ของการเป็นฮีโร่'
   },
