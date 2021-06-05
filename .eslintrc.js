e5be56cc529840f5933914868b7b04c24cb65289
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-05 11:58:47 +0700

    [Yarn] Streamline global lint; Remove lerna

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: { es2021: true, node: true, browser: true, jest: true },
   extends: ['.eslintrc-js.js'],
-  ignorePatterns: ['**/node_modules', '**/dist', '**/lib', '.*/**/*'],
+  ignorePatterns: ['**/.*/**/*', '**/node_modules', '**/dist', '**/lib'],
   overrides: [
     { files: ['*.ts'], extends: ['.eslintrc-ts.js'] },
     { files: ['*.tsx', '*.vue'], extends: ['.eslintrc-vue.js'] }
