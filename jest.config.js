ee6fb46c84de0303edc7441d862dba6bc60e3c04
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-05 14:47:25 +0700

    [Yarn] Streamline global test; Remove lerna

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
-  // transform: { '^.+\\.vue$': 'vue-jest' },
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['/dist/'],
   testPathIgnorePatterns: ['/node_modules/'],
   reporters: ['default'],
-  coverageDirectory: '../coverage'
+  coverageDirectory: './coverage'
 }
