3f82917fbb5c479c4a2bdf223608e3c4666a819c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-06-06 13:18:58 +0700

    [Lint] Restore Vue-lint on component ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
   extends: ['.eslintrc-js.js'],
   ignorePatterns: ['**/.*/**/*', '**/node_modules', '**/dist', '**/lib'],
   overrides: [
-    { files: ['*.ts'], extends: ['.eslintrc-ts.js'] },
-    { files: ['*.tsx', '*.vue'], extends: ['.eslintrc-vue.js'] }
+    { files: ['hosting/src/_services/**/*.ts'], extends: ['.eslintrc-ts.js'] },
+    { files: ['hosting/**/*.ts', 'hosting/**/*.vue'], extends: ['.eslintrc-vue.js'] },
+    { files: ['**/*.ts'], extends: ['.eslintrc-ts.js'] }
   ],
   root: true
 }
